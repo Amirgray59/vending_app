32e5c11ef046b0b8d07df02682a4a384919683f6
AmirGray59 <amiroper59@gmail.com>
2026-07-21 10:55:36 +0330

    feat : Redis

--- app/main.py
+++ app/main.py
@@ -7,6 +7,8 @@ from contextlib import asynccontextmanager
 from app.core.logger import get_logger
 from app.db.session import check_db_connection
 
+from app.db.redis import get_redis 
+
 
 logger = get_logger(__name__)
 
@@ -21,7 +23,14 @@ async def lifespan(app : FastAPI) :
     
     else : 
         logger.info("Database is ok")
-        
+
+    try : 
+        await get_redis().ping() 
+        logger.info("Redis is connected!")
+
+    except Exception as exc : 
+        logger.info(f"Redis is not ok : {exc}")
+
 
     yield
 
